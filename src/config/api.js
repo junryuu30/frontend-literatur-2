d1af0ed5e8087b6767d86e76846ca7b60014c6ed
unknown <jihanfadilah330@gmail.com>
2022-11-16 14:46:52 +0700

    test

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "http://localhost:5000/api/v1/",
+  // baseURL: "http://localhost:5000/api/v1/",
+  baseURL: "https://literatur-server-2.herokuapp.com/api/v1/",
 });
 
 export const setAuthToken = (token) => {
